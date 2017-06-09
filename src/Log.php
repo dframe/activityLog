e84eda35be7bfff62258213ce5c7a39fa50efd17
Sￅﾂawek Kaleta <s.kaleta@citycore.pro>
2017-06-09 15:35:38 +0200

    Prototype activity logs

---
+++ src/Log.php
@@ -0,0 +1,36 @@
+<?php
+namespace Dframe\activityLog;
+
+/**
+ * Dframe/activityLog
+ * Copyright (c) Sławomir Kaleta
+ * @license https://github.com/dusta/activityLog/blob/master/LICENCE
+ *
+ */
+
+class Log
+{
+	
+    public function __construct($driver = false){
+    	$this->driver = $driver;
+    }
+
+    public function typesActivity(array $list){
+
+    }
+
+    public function entity($name){
+    	$this->entity = $name;
+    	return $this;
+    }
+
+    public function modification(array $data){
+    	$this->modification = $data;
+    	return $this;
+    }
+
+    public function log(string $data){
+    	$this->log = $data;
+    }
+
+}
