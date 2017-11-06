ecf7cd82c664888d2e53675df724d8c01cb9c447
Dusta <dusta@users.noreply.github.com>
2017-11-06 14:17:23 +0100

    Update delete.php

--- example/app/Libs/Extensions/activityLog/delete.php
+++ example/app/Libs/Extensions/activityLog/delete.php
@@ -1,5 +1,5 @@
 <?php
-namespace Libs\Extensions\activityLog;
+namespace Libs\Extensions\ActivityLog;
 
 /**
  * Dframe/activityLog
@@ -8,7 +8,7 @@ namespace Libs\Extensions\activityLog;
  *
  */
 
-class delete
+class Delete
 {
 
     public function __construct($before){
